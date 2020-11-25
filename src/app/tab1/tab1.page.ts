2eacae8543194c88c73d7a12ac32c9a10ef9cf06
cedric <cedric.chok@gmail.com>
2020-11-25 11:53:51 +0100

    add test capacitor network plugin

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+// import { Network } from '@ionic-native/network/ngx';
+import { Plugins } from '@capacitor/core';
+
+const { Network } = Plugins;
 
 @Component({
   selector: 'app-tab1',
@@ -7,6 +11,29 @@ import { Component } from '@angular/core';
 })
 export class Tab1Page {
 
-  constructor() {}
+  message = 'Je suis connecté';
+
+  // constructor(private network: Network) {
+  // }
+
+  // disconnect() {
+  //   const disco = this.network.onDisconnect()
+  //       .subscribe(() => {
+  //         this.message = 'J\'ai perdu le réseau :-(';
+  //       });
+  // }
+
+  constructor() {
+    Network.addListener('networkStatusChange', (status) => {
+      console.log(status);
+      if (!status.connected) {
+        this.message = 'Vous êtes déconnecté';
+      } else { this.message = 'Je suis connecté'; }
+    });
+  }
+
+  async start() {
+    const status = await Network.getStatus();
+  }
 
 }
